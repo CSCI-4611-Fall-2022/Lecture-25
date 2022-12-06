fbdc7595b0868b3da9ae0170aecd030d9826a509
Evan Suma Rosenberg <suma@umn.edu>
2022-12-06 13:44:36 -0600

    pre-class commit

---
+++ src/index.ts
@@ -0,0 +1,10 @@
+/* Lecture 25
+ * CSCI 4611, Fall 2022, University of Minnesota
+ * Instructor: Evan Suma Rosenberg <suma@umn.edu>
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { RayCastApp } from './RayCastApp'
+
+const raycastApp = new RayCastApp();
+raycastApp.start();
